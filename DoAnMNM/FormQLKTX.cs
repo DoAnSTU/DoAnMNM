3e11ab40ce22d101d9c798ec8d7ed1e83beb510e
HiDuong <tranhiduong@gmail.com>
2021-12-19 15:45:22 +0700

    Hiển thị danh sách phòng, tìm kiếm phòng

--- DoAnMNM/FormQLKTX.cs
+++ DoAnMNM/FormQLKTX.cs
@@ -133,5 +133,106 @@ namespace DoAnMNM
                 KhoiTao_ThemPhong();
             }
         }
+
+        private void btnDSPhong_Click(object sender, EventArgs e)
+        {
+            tabControlKTX.SelectedTab = tpDanhSachPhong;
+            KhoiTao_DSPhong();
+        }
+
+        private void KhoiTao_DSPhong()
+        {
+            cboLoaiPhong_DSPhong.Items.Clear();
+            cboKhu_DSPhong.Items.Clear();
+            txtMaPhong_DSPhong.Text = "";
+            List<Phong> dsPhong = db.Phongs.ToList();
+            List<LoaiPhong> dsLoaiPhong = db.LoaiPhongs.ToList();
+            foreach (var x in dsLoaiPhong)
+            {
+                cboLoaiPhong_DSPhong.Items.Add(x);
+            }
+
+            List<Khu> dsKhu = db.Khus.ToList();
+            foreach (var x in dsKhu)
+            {
+                cboKhu_DSPhong.Items.Add(x);
+            }
+            HienThiPhong_DSPhong(dsPhong);
+        }
+
+        private void HienThiPhong_DSPhong(List<Phong> dsPhong)
+        {
+            tableLayoutPanelDSPhong.Controls.Clear();
+
+            foreach (var x in dsPhong)
+            {
+                //Tạo picture
+                PictureBox pic = new PictureBox();
+                pic.Name = x.MaPhong;
+                pic.BackColor = System.Drawing.Color.Transparent;
+                pic.Dock = System.Windows.Forms.DockStyle.Fill;
+                pic.Image = global::DoAnMNM.Properties.Resources.home_2;
+                //pic.Image=global::DACNQuanLyKTX.Properties.Resources.
+                pic.Location = new System.Drawing.Point(0, 0);
+                //pic.Size = new System.Drawing.Size(119, 105);
+                pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Normal;
+                pic.TabIndex = 0;
+                pic.TabStop = false;
+                pic.MouseHover += Pic_MouseHover;
+                pic.MouseLeave += Pic_MouseLeave;
+                
+
+
+                //Tạo label
+                Label l = new Label();
+                l.AutoSize = true;
+                l.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                l.ForeColor = System.Drawing.Color.Gainsboro;
+                l.Location = new System.Drawing.Point(24, 114);
+                l.Size = new System.Drawing.Size(50, 17);
+                l.TabIndex = 1;
+                l.Text = x.MaPhong + ": " + timSoSinhVienTrongPhong(x) + "/" + x.LoaiPhong.TongSoGiuong;
+                l.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                //tạo panel
+                Panel p = new Panel();
+                p.Controls.Add(l);
+                p.Controls.Add(pic);
+                //p.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(56)))), ((int)(((byte)(85)))));
+                p.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                p.Dock = System.Windows.Forms.DockStyle.Fill;
+                p.Location = new System.Drawing.Point(3, 3);
+                p.Size = new System.Drawing.Size(119, 144);
+                p.TabIndex = 0;
+                //thêm panel vào layout
+                int a = tableLayoutPanelDSPhong.Controls.Count;
+
+                tableLayoutPanelDSPhong.Controls.Add(p);
+            }
+        }
+
+        private void Pic_MouseHover(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(56)))), ((int)(((byte)(85)))));
+        }
+
+        private void Pic_MouseLeave(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private int timSoSinhVienTrongPhong(Phong p)
+        {
+            int sum = 0;
+            foreach (var x in p.SinhViens)
+            {
+                //Nếu sinh viên có ít nhất một đơn đăn ký có thời gian hợp lệ (Đang có hiệu lực ở)
+                //Tính ngày hết hạn đơn đăng ký bằng ngày vào cộng với thời gian ở
+                if (x.DonDangKies.Where(a => (a.NgayVao.AddYears(a.ThoiGian)) >= DateTime.Now).Count() > 0)
+                {
+                    sum += 1;
+                }
+            }
+            return sum;
+        }
     }
 }
