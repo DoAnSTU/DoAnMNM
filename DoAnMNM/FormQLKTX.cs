5baf8c64b7671dc9310abeaa7317c3fa5cc4b0b4
HiDuong <tranhiduong@gmail.com>
2021-12-19 16:34:42 +0700

    Thống kê số phòng Thống kê số sinh viên Thống kê số giường trống Thống kê tỉ lệ đóng tiền điện Thống kê tỉ lệ đóng tiền phòng

--- DoAnMNM/FormQLKTX.cs
+++ DoAnMNM/FormQLKTX.cs
@@ -30,7 +30,8 @@ namespace DoAnMNM
             InitializeComponent();
             HideTab();
             this.maQL = maQL;
-            lbQuanLy.Text = db.QuanLies.ToList().Find(a => a.MaQL == this.maQL).HoTenQL;           
+            lbQuanLy.Text = db.QuanLies.ToList().Find(a => a.MaQL == this.maQL).HoTenQL;
+            KhoiTao_ThongKe();
         }
 
         private void HideTab()
@@ -84,6 +85,110 @@ namespace DoAnMNM
             db.SaveChanges();
 
             MessageBox.Show("Đã cập nhật thông tin quản lý");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControlKTX.SelectedTab = tpThongKe;
+            KhoiTao_ThongKe();
+        }
+
+        private void KhoiTao_ThongKe()
+        {
+            lbSoPhong_ThongKe.Text = db.Phongs.Count().ToString();
+            lbSoSV_ThongKe.Text = db.SinhViens.Count().ToString();
+            lbSoGiuong_ThongKe.Text = tinhTongGiuongDaO().ToString() + "/" + tinhTongGiuong().ToString();
+            cboQuy_ThongKe.Items.Clear();
+            for (int i = 1; i <= 4; i++)
+            {
+                cboQuy_ThongKe.Items.Add(i);
+            }
+            cboQuy_ThongKe.SelectedIndex = 0;
+            if (DateTime.Now.Month >= 1 || DateTime.Now.Month <= 3)
+                cboQuy_ThongKe.SelectedIndex = cboQuy_ThongKe.Items.IndexOf(1);
+            if (DateTime.Now.Month >= 4 || DateTime.Now.Month <= 6)
+                cboQuy_ThongKe.SelectedIndex = cboQuy_ThongKe.Items.IndexOf(2);
+            if (DateTime.Now.Month >= 7 || DateTime.Now.Month <= 9)
+                cboQuy_ThongKe.SelectedIndex = cboQuy_ThongKe.Items.IndexOf(3);
+            if (DateTime.Now.Month >= 10 || DateTime.Now.Month <= 12)
+                cboQuy_ThongKe.SelectedIndex = cboQuy_ThongKe.Items.IndexOf(4);
+            txtNam_ThongKe.Text = DateTime.Now.Year.ToString();
+            cboThang_ThongKe.Items.Clear();
+            for (int i = 1; i <= 12; i++)
+            {
+                cboThang_ThongKe.Items.Add(i);
+            }
+            cboThang_ThongKe.SelectedIndex = cboThang_ThongKe.Items.IndexOf(DateTime.Now.Month);
+            txtNam2_ThongKe.Text = DateTime.Now.Year.ToString();
+            HienThi_ChartTienDien((int)cboThang_ThongKe.SelectedItem, int.Parse(txtNam2_ThongKe.Text));
+            HienThi_ChartTienPhong((int)cboQuy_ThongKe.SelectedItem, int.Parse(txtNam_ThongKe.Text));
+        }
+
+        private int tinhTongGiuongDaO()
+        {
+            int sum = 0;
+            foreach (var x in db.Phongs)
+            {
+                sum += timSoSinhVienTrongPhong(x);
+            }
+            return sum;
+        }
+
+        private int tinhTongGiuong()
+        {
+            int sum = 0;
+            foreach (var x in db.Phongs)
+            {
+                sum += x.LoaiPhong.TongSoGiuong;
+            }
+            return sum;
+        }
+
+        private int timSoSinhVienTrongPhong(Phong p)
+        {
+            int sum = 0;
+            foreach (var x in p.SinhViens)
+            {
+                //Nếu sinh viên có ít nhất một đơn đăn ký có thời gian hợp lệ (Đang có hiệu lực ở)
+                //Tính ngày hết hạn đơn đăng ký bằng ngày vào cộng với thời gian ở
+                if (x.DonDangKies.Where(a => (a.NgayVao.AddYears(a.ThoiGian)) >= DateTime.Now).Count() > 0)
+                {
+                    sum += 1;
+                }
+            }
+            return sum;
+        }
+
+        private void HienThi_ChartTienDien(int thang, int nam)
+        {
+
+            //danh sách hóa đơn có tháng và năm như biến
+            List<HoaDonDien> ds = db.HoaDonDiens.Where(a => (a.Thang == thang) && (a.Nam == nam)).ToList();
+            int soHDNo = 0;
+            foreach (var x in ds)
+            {
+                if (x.TinhTrang == false)
+                {
+                    soHDNo++;
+                }
+            }
+            chartTienDien_ThongKe.Series["s1"].Points.AddXY("Nợ", soHDNo);
+            chartTienDien_ThongKe.Series["s1"].Points.AddXY("Thanh toán", ds.Count() - soHDNo);
+        }
+
+        private void HienThi_ChartTienPhong(int quy, int nam)
+        {
+            List<HoaDonTienPhong> ds = db.HoaDonTienPhongs.Where(a => (a.Quy == quy) && (a.Nam == nam)).ToList();
+            int soHDNo = 0;
+            foreach (var x in ds)
+            {
+                if (x.TinhTrang == false)
+                {
+                    soHDNo++;
+                }
+            }
+            chartTienPhong_ThongKe.Series["s1"].Points.AddXY("Nợ", soHDNo);
+            chartTienPhong_ThongKe.Series["s1"].Points.AddXY("Thanh toán", ds.Count() - soHDNo);
         }
     }
 }
